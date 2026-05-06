60cb3e0cee4d2309d68489098feb96aec6b7cb6f
Raphael Rüede <raphael.rueede@icloud.com>
2026-05-06 19:22:21 +0200

    Initial Python practice programs

---
+++ calculator.py
@@ -0,0 +1,5 @@
+from math import sqrt
+def root(number):
+    root = sqrt(number)
+    return root
+print(root(int(input())))
---
+++ main.py
@@ -0,0 +1,3 @@
+print("Hello from Ubuntu Linux!")
+name = input("What is your name? ")
+print(f"Welcome, {name}!")
